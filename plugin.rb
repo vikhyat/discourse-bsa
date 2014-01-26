d4bbe5cb2a15c4a3d07c7f3b76dd6c46f6d49ad9
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-01-26 13:22:11 +0530

    initial commit.

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: discourse-bsa
+# about: BuySellAds units for Discourse
+# version: 0.1
+# authors: Vikhyat Korrapati
+
+register_asset "javascripts/ad_unit_component.js"
+
+register_asset "javascripts/discourse/templates/discovery/topics.js.handlebars"
+register_asset "javascripts/discourse/templates/topic.js.handlebars"
+register_asset "javascripts/discourse/templates/user/user.js.handlebars"
