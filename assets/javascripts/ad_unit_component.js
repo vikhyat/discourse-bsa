0d676bf02b0e8933b309527558741009b64fb377
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-04-26 18:54:57 +0530

    Insert ad units on didInsertElement instead of overriding templates.

--- assets/javascripts/ad_unit_component.js
+++ assets/javascripts/ad_unit_component.js
@@ -1,4 +1,12 @@
-Discourse.AdUnitComponent = Ember.Component.extend({
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = 'http://s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+
+var AdUnitComponent = Ember.Component.extend({
   adId: "1293313",
   adClass: "257f81e798bd68dd81e60f42838f361f",
   classNames: ['ad-unit', 'container'],
@@ -10,4 +18,17 @@ Discourse.AdUnitComponent = Ember.Component.extend({
     this.$().html("<div id='" + this.get('divId') + "' class='" + this.get('divClass') + "'></div>");
     if (typeof(_bsap) !== "undefined") { _bsap.exec(); }
   }
+});
+
+Discourse.DiscoveryTopicsView.reopen({
+  injectAd: function() {
+    var self = this;
+
+    self.$(".ad-unit").remove();
+    var AdUnit = self.createChildView(AdUnitComponent);
+
+    AdUnit._insertElementLater(function() {
+      self.$().prepend( AdUnit.$() );
+    });
+  }.on('didInsertElement')
 });
--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,4 @@
 # authors: Vikhyat Korrapati
 
 register_asset "javascripts/ad_unit_component.js"
-
-register_asset "javascripts/discourse/templates/discovery/topics.js.handlebars"
-register_asset "javascripts/discourse/templates/topic.js.handlebars"
-register_asset "javascripts/discourse/templates/user/user.js.handlebars"
+register_asset "stylesheets/ad_unit.scss"
