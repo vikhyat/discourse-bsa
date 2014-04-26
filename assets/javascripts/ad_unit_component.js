9dec931a7d99f56bb2c7a0cc5c2f85dc0a17bca0
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-04-26 18:25:36 +0530

    Get rid of "use strict" IIFE.

--- assets/javascripts/ad_unit_component.js
+++ assets/javascripts/ad_unit_component.js
@@ -1,18 +1,13 @@
-(function() {
-  "use strict";
+Discourse.AdUnitComponent = Ember.Component.extend({
+  adId: "1293313",
+  adClass: "257f81e798bd68dd81e60f42838f361f",
+  classNames: ['ad-unit', 'container'],
 
-  Discourse.AdUnitComponent = Ember.Component.extend({
-    adId: "1293313",
-    adClass: "257f81e798bd68dd81e60f42838f361f",
-    classNames: ['ad-unit', 'container'],
+  divId: function() { return "bsap_" + this.get('adId'); }.property('adId'),
+  divClass: function() { return "bsarocks bsap_" + this.get('adClass'); }.property('adClass'),
 
-    divId: function() { return "bsap_" + this.get('adId'); }.property('adId'),
-    divClass: function() { return "bsarocks bsap_" + this.get('adClass'); }.property('adClass'),
-
-    didInsertElement: function() {
-      this.$().html("<div id='" + this.get('divId') + "' class='" + this.get('divClass') + "'></div>");
-      if (typeof(_bsap) !== "undefined") { _bsap.exec(); }
-    }
-  });
-
-})();
+  didInsertElement: function() {
+    this.$().html("<div id='" + this.get('divId') + "' class='" + this.get('divClass') + "'></div>");
+    if (typeof(_bsap) !== "undefined") { _bsap.exec(); }
+  }
+});
